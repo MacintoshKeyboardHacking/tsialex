549853f15184d9e2fd851a8d989441f38ba5f568
MacintoshKeyboardHacking <MacintoshKeyboardHacking@outlook.com>
2024-08-28 18:17:04 -0400

    add link to descriptive video
    https://youtu.be/rY2Qp2z8-5g

--- 60dollars.sh
+++ 60dollars.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# 240828: inital poc, please see https://youtube.com/@MacintoshKeyboardHacking
+# 240828: inital poc, please see https://youtu.be/rY2Qp2z8-5g (https://youtube.com/@MacintoshKeyboardHacking)
 
 cp MP51.bin myfw-rebuilt.bin
 dd if=myfw-dirtydump.bin of=myfw-rebuilt.bin conv=notrunc bs=65536 skip=20 seek=20 count=1
