da160e4e87687a6823546232ddf592735109f946
MacintoshKeyboardHacking <MacintoshKeyboardHacking@outlook.com>
2024-08-28 11:40:36 -0400

    Add files via upload

---
+++ 60dollars.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# 240828: inital poc, please see https://youtube.com/@MacintoshKeyboardHacking
+
+cp MP51.bin myfw-rebuilt.bin
+dd if=myfw-dirtydump.bin of=myfw-rebuilt.bin conv=notrunc bs=65536 skip=20 seek=20 count=1
+dd if=myfw-dirtydump.bin of=myfw-rebuilt.bin conv=notrunc bs=128 skip=32766 seek=32766 count=1
